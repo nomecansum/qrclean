b7b2884e300362b465a6e4c9924b6dd83002e211
Manuel Ferrero <manuel.ferreroleonardo@telefonica.com>
2020-05-23 11:22:08 +0200

    Quitada url qr

--- resources/views/puestos/print_qr.blade.php
+++ resources/views/puestos/print_qr.blade.php
@@ -32,7 +32,7 @@
             <div class="float-left mb-4 mr-4" style="width: 200px; display: inline-block;">
                 <div class="mb-0 pb-0">
                     <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate(config('app.url_base_scan').$puesto->token)) !!} ">
-                    {{config('app.url_base_scan').$puesto->token}}
+                    {{--  {{config('app.url_base_scan').$puesto->token}}  --}}
                 </div>
                 <div class="w-100 bg-white text-center font-bold mt-0 pb-2" style="color: {{$puesto->val_color}}; background-color: #fff">
                     <i class="{{$puesto->val_icono}}"></i>  {{$puesto->des_puesto}}

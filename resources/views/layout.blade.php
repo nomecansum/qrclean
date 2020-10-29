565c49735dda89492d8a8b4300cbe125f642e88c
ManuelFerrero <nomecansum@gmail.com>
2020-10-29 14:29:08 +0100

    APP_NAME

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>QRClean</title>
+    <title>{{ config('app.name') }}</title>
     <!--STYLESHEET-->
     <!--=================================================-->
     <!--Open Sans Font [ OPTIONAL ]-->

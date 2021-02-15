e93fe4a8fab9690bc348a5269c500a3d2b1155d1
ManuelFerrero <nomecansum@gmail.com>
2021-02-15 13:57:56 +0100

    Otra vez

---
+++ app/Http/Middleware/ForceSSL.php
@@ -0,0 +1,30 @@
+namespace App\Http\Middleware;
+
+use Closure;
+
+use Illuminate\Http\Request;
+
+class ForceSSL
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!app()->environment('local')) {
+            // for Proxies
+            Request::setTrustedProxies([$request->getClientIp()], 
+                Request::HEADER_X_FORWARDED_ALL);
+
+            if (!$request->isSecure()) {
+                return redirect()->secure($request->getRequestUri());
+            }
+        }
+
+        return $next($request);
+    }
+}

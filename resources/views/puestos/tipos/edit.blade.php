d0a4877c7e687cb00df8cd012c2933ad42de8b7d
ManuelFerrero <nomecansum@gmail.com>
2021-02-28 19:10:09 +0100

    Editor de maximo de usos de un detrerminado tipo de puesto Añadida tarea a cholon de liberar reservas

--- resources/views/puestos/tipos/edit.blade.php
+++ resources/views/puestos/tipos/edit.blade.php
@@ -74,6 +74,10 @@
                         <label class="custom-control-label"   for="mca_fijo">Fijo</label>
                     </div>
                     @endif
+                    <div class="form-group col-md-1" style="margin-top: 7px">
+                        <label for="max_usos">Usos simultaneo</label><br>
+                        <input type="number" autocomplete="off" min="1" max="20" name="max_usos" id="max_usos"  class="form-control" value="{{isset($tipo->max_usos)?$tipo->max_usos:1}}" />
+                    </div>
                 </div>
                 
 

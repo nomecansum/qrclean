5708ad464aa43587dfb631a55a2af4802f08d29d
ManuelFerrero <nomecansum@gmail.com>
2022-09-24 13:32:21 +0200

    reserve de salas

--- resources/views/salas/fill_sala.blade.php
+++ resources/views/salas/fill_sala.blade.php
@@ -35,8 +35,8 @@
                 {{ $sala->obs_sala }}
             </div>
         </div>
-        <div class="row">
-            <div style="border: 1px solid #999; width: 100%; height: 20px; border-radius: 3px; background-color: #ddf7c5">
+        <div>
+            <div style="border: 1px solid #999; width: 100%; height: 20px; border-radius: 3px; background-color: #ddf7c5; position: relative">
                 @foreach($reserva_sala as $res)
                     <div class="add-tooltip" title="Reservada de {{ Carbon::parse($res->fec_reserva)->format('H:i') }} a {{ Carbon::parse($res->fec_fin_reserva)->format('H:i') }} @if(config_cliente('mca_mostrar_nombre_usando')=='S') por {{ $res->name }} @endif" 
                         style="background-color: {{ $res->id_usuario==Auth::user()->id?'#ffd700':'#cd5c5c' }};border-radius: 2px; position: absolute; height: 18px; width: {{ 100*(Carbon::parse($res->fec_fin_reserva)->diffinMinutes(Carbon::parse($res->fec_reserva)))/1440 }}%; left: {{ 100*(Carbon::parse($res->fec_reserva)->secondsSinceMidnight()/60)/1440 }}%">
@@ -46,11 +46,10 @@
             
            
         </div>
-        <div class="row nowrap">
+        <div class="nowrap d-flex flex-row">
             @for($n=0;$n<=23;$n++)
                 <div class="text-nowrap" style="width: {{ 100/24 }}%; height: 10px; font-size: 10px; color: #ccc; border-left: 1px solid #efefef; text-align: center; padding-left: 5px">{{ $n }}</div>
             @endfor
-           
         </div>
     </div>
 </div>

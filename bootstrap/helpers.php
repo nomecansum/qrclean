f20cda2ca612b568b94caf39760fd6119b866aa0
ManuelFerrero <nomecansum@gmail.com>
2022-12-07 12:47:12 +0100

    Versoin en footer

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1673,3 +1673,14 @@ function enviar_request_salas($metodo,$accion,$param,$body,$id_cliente){
         "status"=>$response->status()
        ];
 }
+
+function gitVersion()
+{
+    exec('git describe --always',$version_mini_hash);
+    exec('git rev-list HEAD | wc -l',$version_number);
+    exec('git log -1',$line);
+    dd($version_number);
+    // $version['short'] = "v1.".trim($version_number[0]);//.".".$version_mini_hash[0];
+    // $version['full'] = "v1.".trim($version_number[0]).".$version_mini_hash[0] (".str_replace('commit ','',$line[0]).")";
+    return $version;
+}
